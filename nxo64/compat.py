944cd1f680d8a153f48aef57694fd3c36f25cdcd
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-10-12 00:19:46 +0200

    Add compat function get_ord

--- nxo64/compat.py
+++ nxo64/compat.py
@@ -6,9 +6,11 @@ if sys.version_info[0] > 2:
     ascii_string = lambda b: b.decode('ascii')
     bytes_to_list = lambda b: list(b)
     list_to_bytes = lambda l: bytes(l)
+    get_ord = lambda b: b
 else:
     iter_range = xrange
     int_types = (int, long)
     ascii_string = lambda b: str(b)
     bytes_to_list = lambda b: map(ord, b)
     list_to_bytes = lambda l: ''.join(map(chr, l))
+    get_ord = lambda b: ord(b)
